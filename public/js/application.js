45d0833407885af912f95382c5dae57d74c52c4a
Armen Vartan & Justin Lee <pair+armenvartan.leeju@devbootcamp.com>
2014-07-25 00:22:08 -0400

    further js refactor

--- public/js/application.js
+++ public/js/application.js
@@ -1,62 +1,58 @@
 $(document).ready(function(){
-  var game = function() {
-    var player1 = $(document).on('keypress', function(e){
-      console.log("hi")
-      e.preventDefault();
-      var activeTd = $('#player1_strip td.active');
-      var lastTd = $('#player1_strip td').last();
 
-      if ( e.which === 97 ){
-        $(activeTd).next().addClass('active');
-        $(activeTd).removeClass('active');
-      }
-      if( lastTd.hasClass('active') ){
-        gameOver(1);
-      }
+  var movePlayer = function(playerName, keyCode, event){
+    var activeTd = $('#'+playerName+'_strip td.active');
+    var lastTd = $('#'+playerName+'_strip td').last();
+
+    if ( event.which === keyCode ){
+      $(activeTd).next().addClass('active');
+      $(activeTd).removeClass('active');
+    }
+    if( lastTd.hasClass('active') ){
+      gameOver(playerName);
+    }
+  }
+
+  var game = function() {
+    var player1 = $(document).on('keypress', function(event){
+      event.preventDefault();
+      movePlayer("player1", 97, event)
     });
 
-    var player2 = $(document).keypress(function(e){
-      e.preventDefault();
-      var activeTd = $('#player2_strip td.active');
-      var lastTd = $('#player2_strip td').last();
-      if( e.which === 112 ){
-        $(activeTd).next().addClass('active');
-        $(activeTd).removeClass('active');
-      }
-      if( lastTd.hasClass('active') ){
-        gameOver(2);
-      }
+    var player2 = $(document).keypress(function(event){
+      event.preventDefault();
+      movePlayer("player2", 112, event)
     })
+  }
 
-    var gameOver = function(playerNumber){
-      $(document).off();
-      alert("Player "+playerNumber+" has won!");
-      var question = confirm("Restart?") ;
-      if (question == true){
-        restartGame();
-      }else{
-        $.ajax({
-          type: "POST",
-          url: '/game',
-          // data: "hello",
-          success: function(data){
-            $('body').html(data)
-          },
-          fail: function(){
-            alert("There was an error. BACK TO THE SANDBOX")
-            $.get('/')
-          }
-        })
-      }
+  var gameOver = function(playerName){
+    $(document).off();
+    alert(playerName+" has won!");
+    var question = confirm("Restart?") ;
+    if (question == true){
+      restartGame();
+    }else{
+      $.ajax({
+        type: "POST",
+        url: '/game',
+        // data: "hello",
+        success: function(data){
+          $('body').html(data)
+        },
+        fail: function(){
+          alert("There was an error. BACK TO THE SANDBOX")
+          $.get('/')
+        }
+      })
     }
+  }
 
-    var restartGame = function(){
-      $('#player1_strip td.active').removeClass('active');
-      $('#player2_strip td.active').removeClass('active');
-      $('#player1_strip td:nth-child(2)').addClass('active');
-      $('#player2_strip td:nth-child(2)').addClass('active');
-      game();
-    }
+  var restartGame = function(){
+    $('#player1_strip td.active').removeClass('active');
+    $('#player2_strip td.active').removeClass('active');
+    $('#player1_strip td:nth-child(2)').addClass('active');
+    $('#player2_strip td:nth-child(2)').addClass('active');
+    game();
   }
 
   $('button#start_game').on('click', function(e){
