88ec71eb461c7ea60212d1ef87ce2b21af6a06bd
Armen Vartan & Justin Lee <pair+armenvartan.leeju@devbootcamp.com>
2014-07-24 22:29:40 -0400

    session routes

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -2,6 +2,29 @@ get '/login' do
   erb :login
 end
 
+post '/login' do
+  @user = User.find_by(email: params[:email])
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = @user.errors
+    erb :login
+  end
+end
+
+post '/signup' do
+  @user = User.new(params[:user])
+  if @user.valid?
+    @user.save
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = @user.errors
+    erb :login
+  end
+end
+
 get 'logout' do
   session.clear
   redirect '/login'
