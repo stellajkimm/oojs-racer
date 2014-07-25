cdeca540e3231b1c91cfccfbd20271fa9525ce1c
Armen Vartan & Justin Lee <pair+armenvartan.leeju@devbootcamp.com>
2014-07-24 21:50:36 -0400

    fixing models and migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :scores
   has_many :games, through: :scores
+
+  validates_uniqueness_of :name, :email
 end
--- db/migrate/20140724214120_create_played_games.rb
+++ db/migrate/20140724214120_create_played_games.rb
@@ -1,4 +1,11 @@
 class CreatePlayedGames < ActiveRecord::Migration
   def change
+    create_table :played_games do |t|
+      t.references :user
+      t.references :game
+      t.integer :score
+
+      t.timestamps
+    end
   end
 end
