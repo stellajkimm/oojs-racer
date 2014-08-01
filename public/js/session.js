402a0b248aa0e3d3110d4d882be85d5212b794c7
Armen Vartan <vartan.armen@gmail.com>
2014-08-01 00:19:16 -0400

    modal stylez

--- public/js/session.js
+++ public/js/session.js
@@ -8,13 +8,11 @@ $(document).ready(function(){
     // $('#test').hide();
     $('.login-modal').css('visibility', 'visible');
     $('#container').css('opacity', '0.5')
-    // $('.login-form:first-child').attr('value', 'user_one')
   })
   $('#test2').on('click', function(e){
     // $('#test').hide();
     $('.login-modal2').css('visibility', 'visible');
     $('#container').css('opacity', '0.5')
-    // $('.login-form:first-child').attr('value', 'user_two')
   })
   // $('#test1').on('click', function(e){
   //   $('#test').hide();
