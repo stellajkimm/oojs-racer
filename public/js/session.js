703a98fb157a96c3491adde9930db70dcfcde67e
Armen Vartan <vartan.armen@gmail.com>
2014-08-01 00:31:41 -0400

    container now goes back to normal after modals disappear

--- public/js/session.js
+++ public/js/session.js
@@ -14,9 +14,30 @@ $(document).ready(function(){
     $('.login-modal2').css('visibility', 'visible');
     $('#container').css('opacity', '0.5')
   })
-  // $('#test1').on('click', function(e){
-  //   $('#test').hide();
-  //   $('#login-modal').css('visibility', 'visible');
-  //   $('#container').css('opacity', '0.5')
-  // })
+  $('.login-form').on('submit', function(e){
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/login",
+      data: $(this).serialize()
+    }).done(function(response){
+      $('.login-modal').css('visibility', 'hidden')
+      if($('.login-form2').css('visibility') == 'hidden'){
+        $('#container').css('opacity', '1')
+      }
+    })
+  })
+  $('.login-form2').on('submit', function(e){
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/login",
+      data: $(this).serialize()
+    }).done(function(response){
+      $('.login-modal2').css('visibility', 'hidden')
+      if($('.login-form').css('visibility') == 'hidden'){
+        $('#container').css('opacity', '1')
+      }
+    })
+  })
 })
